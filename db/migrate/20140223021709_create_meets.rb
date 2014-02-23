0a23ee46f5f20343cf8dabb6975932d44b66bd4f
Noah Kaplan <noahkaplan4@gmail.com>
2014-02-23 12:44:07 -0800

    changes to migrations

--- db/migrate/20140223021709_create_meets.rb
+++ db/migrate/20140223021709_create_meets.rb
@@ -1,12 +1,12 @@
 class CreateMeets < ActiveRecord::Migration
   def change
     create_table :meets do |t|
-      t.string :comment
-      t.integer :time
+      t.text :comment
+      t.time :time
+      t.date :date
       t.string :address
       t.float :lat
-      t.float :lgn
-      t.belongs_to :user, index: true
+      t.float :lng
 
       t.timestamps
     end
