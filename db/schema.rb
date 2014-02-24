6cd69e520026d8919502c88d05bb9b5167776f06
Noah Kaplan <noahkaplan4@gmail.com>
2014-02-24 10:57:24 -0800

    adding user to meets

--- db/schema.rb
+++ db/schema.rb
@@ -44,12 +44,9 @@ ActiveRecord::Schema.define(version: 20140223215115) do
     t.string   "address"
     t.float    "lat"
     t.float    "lng"
-    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "meets", ["user_id"], name: "index_meets_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
